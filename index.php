50136bfc1e84ef87726368bb06c532d2010c8ec7
Ethan Huang <cherub0526@gmail.com>
2016-08-24 13:56:57 +0800

    update

--- index.php
+++ index.php
@@ -8,8 +8,8 @@ echo <<<EOF
     echo "Service @ \$current_datetime:".file_get_contents('/etc/hostname');
 EOF;
 
-// shell_exec( 'echo "<?php while(1) ; " | php > /dev/null 2>&1 &' );
+shell_exec( 'echo "<?php while(1) ; " | php > /dev/null 2>&1 &' );
 
-// @date_default_timezone_set("Asia/Taipei");
-// $current_datetime = @date('Y-m-d H:i:s', @time());
-// echo "Service @ $current_datetime:".file_get_contents('/etc/hostname')."\n";
+@date_default_timezone_set("Asia/Taipei");
+$current_datetime = @date('Y-m-d H:i:s', @time());
+echo "Service @ $current_datetime:".file_get_contents('/etc/hostname')."\n";
